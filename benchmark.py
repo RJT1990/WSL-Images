e8f29e2a3367c0d8b9eb346118d36fa2bd6eb877
Ross Taylor <rj-taylor@live.co.uk>
2019-07-26 21:46:04 +0100

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -14,11 +14,11 @@ b0_input_transform = transforms.Compose([
 
 # Run Evaluation
 ImageNet.benchmark(
-    model=torch.hub.load('facebookresearch/WSL-Images', 'resnext101_32x16d_wsl'),
+    model=torch.hub.load('facebookresearch/WSL-Images', 'resnext101_32x48d_wsl'),
     paper_model_name='ResNeXt-101 32x48d',
     paper_arxiv_id='1805.00932',
     paper_pwc_id='exploring-the-limits-of-weakly-supervised',
     input_transform=b0_input_transform,
-    batch_size=256,
+    batch_size=64,
     num_gpu=1
 )
