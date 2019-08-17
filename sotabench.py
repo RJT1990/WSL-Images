6b6f1f5fc990bc3df12df8307752500775692ef1
Ross Taylor <rj-taylor@live.co.uk>
2019-08-17 14:21:57 +0100

    Update and rename benchmark.py to sotabench.py

--- sotabench.py
+++ sotabench.py
@@ -22,3 +22,25 @@ ImageNet.benchmark(
     batch_size=64,
     num_gpu=1
 )
+
+# Run Evaluation
+ImageNet.benchmark(
+    model=torch.hub.load('facebookresearch/WSL-Images', 'resnext101_32x32d_wsl'),
+    paper_model_name='ResNeXt-101 32x32d',
+    paper_arxiv_id='1805.00932',
+    paper_pwc_id='exploring-the-limits-of-weakly-supervised',
+    input_transform=b0_input_transform,
+    batch_size=128,
+    num_gpu=1
+)
+
+# Run Evaluation
+ImageNet.benchmark(
+    model=torch.hub.load('facebookresearch/WSL-Images', 'resnext101_32x16d_wsl'),
+    paper_model_name='ResNeXt-101 32x16d',
+    paper_arxiv_id='1805.00932',
+    paper_pwc_id='exploring-the-limits-of-weakly-supervised',
+    input_transform=b0_input_transform,
+    batch_size=256,
+    num_gpu=1
+)
